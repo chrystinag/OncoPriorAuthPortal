5f98e377b11b6660f9d220b9f6747197bc016514
chrystinag <gonzalez.chrystina@gmail.com>
2025-05-11 11:23:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import { AuthProvider } from "./AuthProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
