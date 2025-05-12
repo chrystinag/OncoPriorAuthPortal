6536db27ce829dd50bcf874e2d2dd89cd8d280bc
chrystinag <gonzalez.chrystina@gmail.com>
2025-05-11 22:58:28 -0400

    Update SubmitPA.js
    Update

--- src/pages/SubmitPA.js
+++ src/pages/SubmitPA.js
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://wzenvycwzxitcmusaxak.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6ZW52eWN3enhpdGNtdXNheGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNDMzNjgsImV4cCI6MjA2MTgxOTM2OH0.sbrjDCHlvPPKQkkHdF63lvDkWuCXKPD2gI3zPOdzYRo"
 );
 
 export default function SubmitPA() {
